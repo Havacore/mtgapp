58156043aacccd84673fc8dfc0af821d8eb815a9
jdahl <jdahl@vendasta.com>
2016-05-27 08:00:29 -0600

    got angular material 2 working

--- src/static/app/app.ts
+++ src/static/app/app.ts
@@ -1,9 +1,12 @@
-import {Component} from "@angular/core/src/metadata";
-import {OnInit} from "@angular/core/src/metadata/lifecycle_hooks";
+import { Component } from "@angular/core/src/metadata";
+import { OnInit } from "@angular/core/src/metadata/lifecycle_hooks";
+
+import  { MdSidenav, MD_SIDENAV_DIRECTIVES } from "@angular2-material/sidenav";
 
 @Component({
-    selector: "my-app",
-    template: `<h1>It's Finally working!!!</h1>`
+    selector: "dashboard-component",
+    directives: [ MdSidenav, MD_SIDENAV_DIRECTIVES ],
+    templateUrl: "templates/dashboard.component.html"
 })
 export class AppComponent implements OnInit {
 
--- src/static/systemjs.config.js
+++ src/static/systemjs.config.js
@@ -3,11 +3,14 @@ System.config({
     typescriptOptions: {emitDecoratorMetadata: true},
     'map' : {
         'rxjs': '/node_modules/rxjs',
-        '@angular' : '/node_modules/@angular'
+        '@angular' : '/node_modules/@angular',
+        '@angular2-material': 'node_modules/@angular2-material',
     },
     'packages' : {
         'app': {main: 'main', dafaultExtension: 'js' },
         'rxjs': {main: 'index.js'},
+        '@angular2-material/core': { format: 'cjs', defaultExtension: 'js', main: 'core.js' },
+        '@angular2-material/sidenav': { format: 'cjs', defaultExtension: 'js', main: 'sidenav.js' },
         '@angular/core': {main: 'index.js'},
         '@angular/common': {main: 'index.js'},
         '@angular/compiler': {main: 'index.js'},
